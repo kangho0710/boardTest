e4c5bf5c14d5ee258c7aa66c3ff05b2ccf5b2947
user <user@i7B-29>
2023-01-16 14:28:47 +0900

    01/16

--- src/main/java/com/hellow/config/WebMvcConfig.java
+++ src/main/java/com/hellow/config/WebMvcConfig.java
@@ -8,11 +8,11 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class WebMvcConfig implements WebMvcConfigurer {
 
 	//web root가 아닌 외부 경로에 있는 리소스를 url로 불러올 수 있도록 설정
-    //현재 localhost:8090/summernoteImage/1234.jpg
+    //현재 localhost/summernoteImage/1234.jpg
     //로 접속하면 D:/work/image/1234.jpg 파일을 불러온다.
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
         registry.addResourceHandler("/summernoteImage/**")
-                .addResourceLocations("file:///D:/work/");
+                .addResourceLocations("file:///C:/java-work//");
     }
 }
--- src/main/java/com/hellow/service/TestBoardService.java
+++ src/main/java/com/hellow/service/TestBoardService.java
@@ -1,10 +1,18 @@
 package com.hellow.service;
 
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
 import java.util.List;
+import java.util.UUID;
 
+import org.apache.commons.io.FileUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
 
+import com.google.gson.JsonObject;
 import com.hellow.mapper.TestBoardMapper;
 import com.hellow.vo.TestBoardVO;
 
@@ -36,6 +44,32 @@ public class TestBoardService {
 	
 	public int count() {
 		return testBoardMapper.count();
+	}
+	
+	public JsonObject uploadSummernoteImageFile(@RequestParam("file") MultipartFile multipartFile) {
+		JsonObject jsonObject = new JsonObject();
+
+		String fileRoot = "C:\\java-work\\"; // 저장될 외부 파일 경로
+		String originalFileName = multipartFile.getOriginalFilename(); // 오리지날 파일명
+		String extension = originalFileName.substring(originalFileName.lastIndexOf(".")); // 파일 확장자
+
+		String savedFileName = UUID.randomUUID() + extension; // 저장될 파일 명
+
+		File targetFile = new File(fileRoot + savedFileName);
+
+		try {
+			InputStream fileStream = multipartFile.getInputStream();
+			FileUtils.copyInputStreamToFile(fileStream, targetFile); // 파일 저장
+			jsonObject.addProperty("url", "/summernoteImage/" + savedFileName);
+			jsonObject.addProperty("responseCode", "success");
+
+		} catch (IOException e) {
+			FileUtils.deleteQuietly(targetFile); // 저장된 파일 삭제
+			jsonObject.addProperty("responseCode", "error");
+			e.printStackTrace();
+		}
+
+		return jsonObject;
 	}
 	
 }
